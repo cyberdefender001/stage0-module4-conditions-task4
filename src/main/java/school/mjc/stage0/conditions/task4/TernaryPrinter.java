9288c607b3726035a72a9c5075a64622bbbda654
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-31 12:10:59 +0500

    Update TernaryPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
+        int max = 0;
+        first > second?
+            max = first:
+            max = second;
+        System.out.println(max);
     }
 }
