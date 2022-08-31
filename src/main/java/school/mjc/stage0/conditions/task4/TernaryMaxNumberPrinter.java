edb55fcb7779f767f4b83a051945535acc91bc27
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-31 12:26:16 +0500

    Update TernaryMaxNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
-    public void printGreatest(int first, int second, int third) {
+    public static void printGreatest(int first, int second, int third) {
+        int max = 0;
+        int max1 = 0;
+        first > second ?
+            max1 = first;
+            max1 = second;
+        max1 > third ?
+            max = max1:
+            max = third;
+        System.out.println(max);
     }
 }
