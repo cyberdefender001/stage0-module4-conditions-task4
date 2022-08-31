204cafe95fc45a06b5291098a6e8cecb9a6c3aef
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-31 12:15:05 +0500

    Update TernaryAssignment.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -1,6 +1,11 @@
 package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
-    public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+    public static void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        int result = 0;
+        first > second?
+            result = 10;
+            result = -10;
+        System.out.println(result);
     }
 }
